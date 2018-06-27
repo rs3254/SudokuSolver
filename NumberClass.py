bb9dc5b2d48a166ab2498d09d6c3e918ca0f4765
rs3254 <raysabb3254@gmail.com>
2018-06-27 17:38:13 -0400

    NumberClass improved

--- NumberClass.py
+++ NumberClass.py
@@ -24,14 +24,48 @@ class NumberClass:
 
 		while self.checkRows(arr1, arr2) == False:
 			arr2 = self.genRows(puzzle[9:18])
+			arr1 = self.genRows(puzzle[0:9])
 
+		arr3 = self.genThirdRow(arr1, arr2, arr3)
 
-		# while self.checkRows(arr2, arr3) == False or self.checkRows(arr1, arr3) == False:
-		# 	arr3 = self.genRows(puzzle[18:27])
-
-
-		mergedArr = arr1 + arr2  
+		mergedArr = arr1 + arr2  + arr3
 		return mergedArr
+
+
+
+
+	def genThirdRow(self, arr1, arr2, puzzle):
+		forbiddenNums1 = arr1[0:3] + arr2[0:3]
+		forbiddenNums2 = arr1[3:6] + arr2[3:6]
+		forbiddenNums3 = arr1[6:9] + arr2[6:9]
+
+
+
+		thirdRow = self.genThirdRowHelper(forbiddenNums1, puzzle) + self.genThirdRowHelper(forbiddenNums2, puzzle) + self.genThirdRowHelper(forbiddenNums3, puzzle)
+		return thirdRow
+
+
+	def genThirdRowHelper(self, arr1, puzzle):
+		newArr = arr1
+		thirdRow = [] 
+		x = randint(1, 9)
+		count = 0
+		j = 0
+		while count < 3:
+			if puzzle[j] == 0:
+				newArr.append(puzzle[j])
+				j += 1
+			elif x not in arr1:
+				thirdRow.append(x)
+				newArr.append(x)
+				x = randint(1, 9)
+				count += 1
+				j += 1
+			else:
+				x = randint(1,9)
+
+
+		return thirdRow
 
 
 
@@ -46,7 +80,6 @@ class NumberClass:
 				if row1[i]  == row2[j]:
 					return False
 
-		
 		for i in range(6, 9):
 			for j in range(6, 9):
 				if row1[i]  == row2[j]:
--- main.py
+++ main.py
@@ -4,47 +4,57 @@ from NumberClass import NumberClass
 
 
 
-raw_html = get_url("https://www.sudokuweb.org")
-html = BeautifulSoup(raw_html, "html.parser")
+# raw_html = get_url("https://www.sudokuweb.org")
+# html = BeautifulSoup(raw_html, "html.parser")
 
 
-{"class":["class1", "class2"]}
+# {"class":["class1", "class2"]}
 
-nums = html.find_all("span", {"class":["true", "sedy"]})
-numsArr = [] 
+# nums = html.find_all("span", {"class":["true", "sedy"]})
+# numsArr = [] 
 
-for j in nums:
-	for z in j:
-		numsArr.append(z)
-
-
-
-
-givenNums = html.find_all("span", class_="true")
-numsArrGiven = [] 
-
-for j in givenNums:
-	for z in j:
-		numsArrGiven.append(z)
+# for j in nums:
+# 	for z in j:
+# 		numsArr.append(z)
 
 
 
-sudokuPuzzle = []
+
+# givenNums = html.find_all("span", class_="true")
+# numsArrGiven = [] 
+
+# for j in givenNums:
+# 	for z in j:
+# 		numsArrGiven.append(z)
+
+
+
+# sudokuPuzzle = []
+# finishedPuzzle = [] 
+
+# count = 0
+# for j in range(0, 81):
+# 	if count > len(numsArrGiven) -1:
+# 		break
+# 	elif numsArr[j] == numsArrGiven[count]:
+# 		sudokuPuzzle.append(numsArr[j])
+# 		count += 1
+# 	else:
+# 		sudokuPuzzle.append(0)
+
+
+
+# print(sudokuPuzzle)
+
+
+
 finishedPuzzle = [] 
 
-count = 0
-for j in range(0, 81):
-	if count > len(numsArrGiven) -1:
-		break
-	elif numsArr[j] == numsArrGiven[count]:
-		sudokuPuzzle.append(numsArr[j])
-		count += 1
-	else:
-		sudokuPuzzle.append(0)
+puzzleT = ['4', 0, '1', 0, 0, '3', '9', 0, '5', 0, 0, 0, '7', '1', '4', '8', '3', 0, 0, '7', '3', '9', 0, 0, '1', 0, '4', '7', '1', 0, '3', '2', 0, '5', '8', '9', '8', '3', '9', '5', 0, '7', 0, 0, '2', 0, '6', 0, 0, 0, '1', '3', 0, 0, 0, '2', '6', '4', '3', 0, '7', 0, '8', 0, '5', 0, '1', '7', '2', '4', '6', 0, 0, 0, 0, 0, 0, 0, '2', 0, '1']
+arrC = list(map(int, puzzleT))
 
-
-
-print(len(sudokuPuzzle))
+# print(len(arrC))
+# print(arrC)
 
 
 rowArr = [] 
@@ -54,7 +64,7 @@ y = 0
 numObj = NumberClass()
 
 for i in range(0, 9):
-	finishedPuzzle += numObj.genRows(sudokuPuzzle[y:x])
+	finishedPuzzle += numObj.genRows(arrC[y:x])
 	y = x 
 	x += 9
 
@@ -65,11 +75,33 @@ for i in range(0, 9):
 
 
 
-# finishedPuzzle = numObj.genBlock(finishedPuzzle, sudokuPuzzle)
+finishedPuzzle = numObj.genBlock(finishedPuzzle, arrC)
 
 
-# print(finishedPuzzle[0:9])
-# print(finishedPuzzle[9:18])
+print(finishedPuzzle[0:9])
+print(finishedPuzzle[9:18])
+print(finishedPuzzle[18:27])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# print(finishedPuzzle[18:27])
+
 
 	
 
@@ -85,14 +117,14 @@ for i in range(0, 9):
 
 # below code checks to see that puzzle created successfully 
 
-recreateArr = [] 
-for i in range(0, len(sudokuPuzzle)):
-	if sudokuPuzzle[i] != 0:
-		recreateArr.append(sudokuPuzzle[i])
+# recreateArr = [] 
+# for i in range(0, len(sudokuPuzzle)):
+# 	if sudokuPuzzle[i] != 0:
+# 		recreateArr.append(sudokuPuzzle[i])
 
 
-if recreateArr == numsArrGiven:
-	print("success")
+# if recreateArr == numsArrGiven:
+# 	print("success")
 
 
 
